17d9446e5ff3ac41b08e4cda507876716c1e0f9f
Agnaldo Junior <acjunior@Agnaldos-MacBook-Pro-3.local>
2022-11-22 17:03:12 +0000

    first commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Agnaldo Junior on 22/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bullseye/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  Bullseye
+//
+//  Created by Agnaldo Junior on 22/11/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+                .bold()
+                .kerning(2.0)
+                .multilineTextAlignment(.center)
+                .lineSpacing(4.0)
+                .font(.footnote)
+            Text("89")
+                .kerning(-1.0)
+                .fontWeight(.black)
+                .font(.largeTitle)
+            HStack {
+                Text("1")
+                    .bold()
+                Slider(value: .constant(50), in: 1.0...100.0)
+                Text("100")
+                    .bold()
+            }
+            Button("Hit me"){
+                print("Hello SwiftUI!")
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+        ContentView()
+            .previewLayout(.fixed(width: 668, height: 320))
+    }
+}
